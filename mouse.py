b76bf0a7c34fec437ed643f313079366019b5e7d
best1a <best1a@laptop.(none)>
2013-01-27 22:42:46 +0800

    make it pep8 style. except e501

--- mouse.py
+++ mouse.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 from evdev import InputDevice, categorize, ecodes, UInput
-import time, os
+import time
+import os
 
 script_name = 'sample'
 dev_path = '/dev/input/event4'
 operations = []
+
 
 def run(ui):
     for operation in operations:
         if operation['op'] == 'TIME':
             time.sleep(operation['value'] / 1000.0)
         else:
-            ui.write(ecodes.EV_KEY, ecodes.ecodes[operation['op']], \
-                operation['value'])
+            ui.write(ecodes.EV_KEY, ecodes.ecodes[operation['op']], operation['value'])
             ui.syn()
 
 try:
@@ -21,9 +22,10 @@ try:
     with open(full_script_name) as f:
         content = f.read()
         for line in content.split('\n'):
-            if line.startswith('#') or len(line) == 0: continue
+            if line.startswith('#') or len(line) == 0:
+                continue
             op, value = line.split(' ')
-            operations.append(dict(op = op, value = int(value)))
+            operations.append(dict(op=op, value=int(value)))
 except IOError:
     print "Can't open", full_script_name
     exit(1)
@@ -36,10 +38,9 @@ try:
     dev.grab()
     ui = UInput()
     for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY and event.code == ecodes.KEY_LEFTMETA\
-        and event.value == 0:
+        if event.type == ecodes.EV_KEY and event.code == ecodes.KEY_LEFTMETA and event.value == 0:
             run(ui)
 except OSError, e:
-    print 'Please run this script with sudo'
+    print 'Please run this script with sudo', e
 except KeyboardInterrupt:
     pass
